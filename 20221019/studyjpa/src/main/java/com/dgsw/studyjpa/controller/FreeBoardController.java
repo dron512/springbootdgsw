d66a95fc1f45e7d3d53144a44afff295f00328cc
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2022-10-26 13:34:54 +0900

    221026

---
+++ 20221019/studyjpa/src/main/java/com/dgsw/studyjpa/controller/FreeBoardController.java
@@ -0,0 +1,48 @@
+package com.dgsw.studyjpa.controller;
+
+
+import com.dgsw.studyjpa.entity.FreeBoard;
+import com.dgsw.studyjpa.repository.FreeBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequestMapping("board")
+@RestController
+public class FreeBoardController {
+
+    @Autowired
+    FreeBoardRepository freeBoardRepository;
+
+    @GetMapping("list")
+    public List<FreeBoard> list(){
+        List<FreeBoard> list = freeBoardRepository.findAll();
+        return list;
+    }
+    @PostMapping("write")
+    public String write(String writer,String content){
+        System.out.println("writer = "+writer);
+        System.out.println("content = "+ content);
+        FreeBoard fb = new FreeBoard();
+        fb.setContent(content);
+        fb.setWriter(writer);
+        fb.setWdate(LocalDateTime.now());
+        freeBoardRepository.save(fb);
+        return "write";
+    }
+    @PostMapping("update")
+    public String update(){
+        return "update";
+    }
+    @PostMapping("delete")
+    public String delete(){
+        return "delete";
+    }
+
+}
---
+++ 20221019/studyjpa/src/main/java/com/dgsw/studyjpa/entity/FreeBoard.java
@@ -0,0 +1,27 @@
+package com.dgsw.studyjpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class FreeBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    
+    private String writer;
+    private String title;
+    private String content;
+    // 작성일자
+    private LocalDateTime wdate;
+    
+
+}
