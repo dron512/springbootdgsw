1c3b24547338cb8a94fc61cbe832c1a5a5aecaee
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2022-11-30 15:07:14 +0900

    h2-console

--- jwtTest/src/main/java/com/example/demo/config/SecuriyConfig.java
+++ jwtTest/src/main/java/com/example/demo/config/SecuriyConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,11 +24,18 @@ public class SecuriyConfig extends WebSecurityConfigurerAdapter
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-//        http.authorizeRequests()
-//                .antMatchers("/sample/all").permitAll()
-//                .antMatchers("/sample/member").hasRole("USER");
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
 
         http.formLogin();
         http.csrf().disable();
---
+++ jwtTest/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name="user1")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String username;
+}
